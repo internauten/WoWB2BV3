de71a5321d39477db54b2edc8e6ca85e02f21252
Daniel Morgenthaler <daniel@internauten.ch>
2026-02-09 21:30:48 +0100

    Translation bereinigt

--- modules/grouppricetext/translations/de.php
+++ modules/grouppricetext/translations/de.php
@@ -3,4 +3,4 @@
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{grouppricetext}prestashop>displayproductpriceblock_c76f82270692bc0df58ccbee423072cc'] = 'Katalogpreis:';
+$_MODULE['<{grouppricetext}prestashop>displayproductpriceblock_c76f82270692bc0df58ccbee423072cc'] = 'Katalogpreis ist:';
