970ad73e4f436db4949c0795cde1ef7df97a4005
Daniel Morgenthaler <daniel@internauten.ch>
2026-02-12 16:44:06 +0100

    All strings translated

--- modules/grouppricetext/translations/de.php
+++ modules/grouppricetext/translations/de.php
@@ -2,9 +2,18 @@
 
 global $_MODULE;
 $_MODULE = array();
-
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_a724ec2d2c355db62a5f469c8c2a4974'] = 'Gruppenpreis-Text';
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_ce87ff5397060ac26b59e43540b505f7'] = 'Benutzerdefinierten Text auf Produktseiten für bestimmte Kundengruppen anzeigen';
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_bb8956c67b82c7444a80c6b2433dd8b4'] = 'Möchten Sie dieses Modul wirklich deinstallieren?';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_19636395066a245172dc03e21c861ca4'] = 'Ungültige Gruppen ID';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_462390017ab0938911d2d4e964c0cab7'] = 'Einstellungen erfolgreich gespeichert';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_f4f70727dc34561dfde1a3c529b6205c'] = 'Einstellungen';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_7a01a554b2fe1ebe7f033013913d66b3'] = 'Modul aktiv';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_8110eae95ded27f8d4c7dd6ea6e4f5d1'] = 'Modul aktivieren oder deaktivieren';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Aktiv';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_b9f5c797ebbf55adccdd8539a65a0241'] = 'Inaktiv';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_08b02383dce834815c99539f02f0fd89'] = 'Gruppe Wiederverkäufer';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_9e43df7c6ddc2519bda266ddc1a812fb'] = 'Wählen sie die Gruppe welche die Wiederverkäufer gesetzt haben.';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_c9cc8cce247e49bae79f15173ce97354'] = 'Speichern';
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_89850fde9cbf081b89b5cfcc5b940ff0'] = 'Kein Sonderpreis für Wiederverkäufer verfügbar! Lassen Sie sich eine Offerte geben.';
 $_MODULE['<{grouppricetext}prestashop>displayproductpriceblock_c76f82270692bc0df58ccbee423072cc'] = 'Katalogpreis ist:';
--- modules/grouppricetext/translations/en.php
+++ modules/grouppricetext/translations/en.php
@@ -2,9 +2,18 @@
 
 global $_MODULE;
 $_MODULE = array();
-
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_a724ec2d2c355db62a5f469c8c2a4974'] = 'Group Price Text';
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_ce87ff5397060ac26b59e43540b505f7'] = 'Display custom text on product pages for specific customer groups';
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_bb8956c67b82c7444a80c6b2433dd8b4'] = 'Are you sure you want to uninstall this module?';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_19636395066a245172dc03e21c861ca4'] = 'Invalid group ID';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_462390017ab0938911d2d4e964c0cab7'] = 'Settings updated successfully';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_f4f70727dc34561dfde1a3c529b6205c'] = 'Settings';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_7a01a554b2fe1ebe7f033013913d66b3'] = 'Enable module';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_8110eae95ded27f8d4c7dd6ea6e4f5d1'] = 'Enable or disable the module';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Enabled';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_b9f5c797ebbf55adccdd8539a65a0241'] = 'Disabled';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_08b02383dce834815c99539f02f0fd89'] = 'Customer Group';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_9e43df7c6ddc2519bda266ddc1a812fb'] = 'Select the customer group that will see the message';
+$_MODULE['<{grouppricetext}prestashop>grouppricetext_c9cc8cce247e49bae79f15173ce97354'] = 'Save';
 $_MODULE['<{grouppricetext}prestashop>grouppricetext_89850fde9cbf081b89b5cfcc5b940ff0'] = 'Special pricing for your group!';
 $_MODULE['<{grouppricetext}prestashop>displayproductpriceblock_c76f82270692bc0df58ccbee423072cc'] = 'Catalog price:';
